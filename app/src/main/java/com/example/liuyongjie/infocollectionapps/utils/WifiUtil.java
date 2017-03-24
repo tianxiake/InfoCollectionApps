df1788a39fc53c503d2dba6992e5f500d5006353
lyjVlife <liuyongjie@vlifepaper.com>
2017-03-24 16:09:23 +0800

    添加notforwork包，用于存放其他的获取信息

--- app/src/main/java/com/example/liuyongjie/infocollectionapps/utils/WifiUtil.java
+++ app/src/main/java/com/example/liuyongjie/infocollectionapps/utils/WifiUtil.java
@@ -1,13 +1,10 @@
 package com.example.liuyongjie.infocollectionapps.utils;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.net.wifi.ScanResult;
 import android.net.wifi.SupplicantState;
-import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import android.os.Build;
 
 import com.example.liuyongjie.infocollectionapps.entity.CustomWifiInfo;
 import com.example.liuyongjie.infocollectionapps.log.LoggerFactory;
@@ -67,12 +64,12 @@ public class WifiUtil {
         return (mWifiInfo == null) ? "NULL" : mWifiInfo.getSSID();
     }
 
-    // 得到wifi的接入点地址
+    // 得到wifi的接入点地址(就是路由器的Mac地址)
     public String getBSSID() {
         return (mWifiInfo == null) ? "NULL" : mWifiInfo.getBSSID();
     }
 
-    // 得到wifi的MAC地址,通过执行本地命令获取
+    // 得到当前wifi设备的MAC地址,通过执行本地命令获取
     public String getMacAddress() {
         Runtime runtime = Runtime.getRuntime();
         try {
@@ -101,129 +98,11 @@ public class WifiUtil {
         return mWifiInfo.getLinkSpeed();
     }
 
-    ////////////////////////////////////////////////////////////////////
-    // 打开WIFI
-    public void openWifi() {
-        if (!mWifiManager.isWifiEnabled()) {
-            mWifiManager.setWifiEnabled(true);
-        }
+    //获取wifi信号的强度
+    public int getwifiStrength() {
+        int rssi = mWifiInfo.getRssi();
+        int strength = WifiManager.calculateSignalLevel(rssi, 5);
+        return strength;
     }
-
-    // 关闭WIFI
-    public void closeWifi() {
-        if (mWifiManager.isWifiEnabled()) {
-            mWifiManager.setWifiEnabled(false);
-        }
-    }
-
-    // 检查当前WIFI状态
-    public int checkState() {
-        return mWifiManager.getWifiState();
-    }
-
-    // 锁定WifiLock
-    public void acquireWifiLock() {
-        mWifiLock.acquire();
-    }
-
-    // 解锁WifiLock
-    public void releaseWifiLock() {
-        // 判断时候锁定
-        if (mWifiLock.isHeld()) {
-            mWifiLock.acquire();
-        }
-    }
-
-    // 创建一个WifiLock
-    public void creatWifiLock() {
-        mWifiLock = mWifiManager.createWifiLock("Test");
-    }
-
-    // 得到配置好的网络
-    public List getConfiguration() {
-        return mWifiConfiguration;
-    }
-
-    public void startScan() {
-        mWifiManager.startScan();
-        // 得到扫描结果
-        mWifiList = mWifiManager.getScanResults();
-        // 得到配置好的网络连接
-        mWifiConfiguration = mWifiManager.getConfiguredNetworks();
-    }
-
-
-    // 查看扫描结果
-    public StringBuilder lookUpScan() {
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int i = 0; i < mWifiList.size(); i++) {
-            stringBuilder
-                    .append("Index_" + new Integer(i + 1).toString() + ":");
-            // 将ScanResult信息转换成一个字符串包
-            // 其中把包括：BSSID、SSID、capabilities、frequency、level
-            stringBuilder.append((mWifiList.get(i)).toString());
-            stringBuilder.append("/n");
-        }
-        return stringBuilder;
-    }
-
-
-    // 得到连接的ID
-    public int getNetworkId() {
-        return (mWifiInfo == null) ? 0 : mWifiInfo.getNetworkId();
-    }
-
-    // 得到WifiInfo的所有信息包
-    public String getWifiInfo() {
-        return (mWifiInfo == null) ? "NULL" : mWifiInfo.toString();
-    }
-
-
-    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
-    //获取当前wifi的连接频率，默认单位是MHz
-    public int getFrequency() {
-        return mWifiInfo.getFrequency();
-    }
-
-    // 添加一个网络并连接
-    public void addNetwork(WifiConfiguration wcg) {
-        int wcgID = mWifiManager.addNetwork(wcg);
-        boolean b = mWifiManager.enableNetwork(wcgID, true);
-        System.out.println("a--" + wcgID);
-        System.out.println("b--" + b);
-    }
-
-    // 断开指定ID的网络
-    public void disconnectWifi(int netId) {
-        mWifiManager.disableNetwork(netId);
-        mWifiManager.disconnect();
-    }
-//
-//    public String getMacAddr() {
-//        try {
-//            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
-//            for (NetworkInterface nif : all) {
-//                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
-//
-//                byte[] macBytes = nif.getHardwareAddress();
-//                if (macBytes == null) {
-//                    return "";
-//                }
-//
-//                StringBuilder res1 = new StringBuilder();
-//                for (byte b : macBytes) {
-//                    res1.append(String.format("%02X:", b));
-//                }
-//
-//                if (res1.length() > 0) {
-//                    res1.deleteCharAt(res1.length() - 1);
-//                }
-//                return res1.toString();
-//            }
-//        } catch (Exception e) {
-//            Log.e("TAG", "获取mac地址失败", e);
-//        }
-//        return "02:00:00:00:00:00";
-//    }
 
 }
