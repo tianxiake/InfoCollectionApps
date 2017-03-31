cd3cad2b12598aa5fcc1262f5a19c18a6bec2fa5
lyjVlife <liuyongjie@vlifepaper.com>
2017-03-31 18:03:36 +0800

    微量修改

---
+++ app/src/test/java/com/example/liuyongjie/infocollectionapps/utils/AppUtilTest.java
@@ -0,0 +1,65 @@
+package com.example.liuyongjie.infocollectionapps.utils;
+
+import org.junit.Test;
+
+/**
+ * Created by liuyongjie on 2017/3/31.
+ */
+public class AppUtilTest {
+
+    @Test
+    public void getAppName() throws Exception {
+
+    }
+
+    @Test
+    public void getVersionName() throws Exception {
+
+    }
+
+    @Test
+    public void getCurrentAppPath() throws Exception {
+
+    }
+
+    @Test
+    public void getAppHashCode() throws Exception {
+
+    }
+
+    @Test
+    public void getPackageResourcePath() throws Exception {
+
+    }
+
+    @Test
+    public void getPackageName() throws Exception {
+
+    }
+
+    @Test
+    public void getSoftVersion() throws Exception {
+
+    }
+
+    @Test
+    public void getLanguage() throws Exception {
+
+    }
+
+    @Test
+    public void getCurProcessName() throws Exception {
+
+    }
+
+    @Test
+    public void getChannelCode() throws Exception {
+
+    }
+
+    @Test
+    public void getInstallAppList() throws Exception {
+
+    }
+
+}
